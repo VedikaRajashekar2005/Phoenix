721d62910a36a8d9258a8aa1a0f0ed4e44a7558a
Varsha V <162611754+varshavkumar12345@users.noreply.github.com>
2025-11-21 20:14:25 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FirewallSimulator from "./FirewallSimulator";
+
+const App = () => {
+  return (
+    <div>
+      <FirewallSimulator />
+    </div>
+  );
+};
+
+export default App;
